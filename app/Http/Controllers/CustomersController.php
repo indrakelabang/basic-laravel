a340daf10e0ab35755b774b89c605840c3591150
gancarajriya <gancarajriya@gmail.com>
2019-06-02 14:20:49 +0700

    add status active n inactive, use compact func

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -9,24 +9,29 @@ class CustomersController extends Controller
 {
     public function list()
     {
+        $activeCustomers = Customer::where('active', 1)->get();
+        $inactiveCustomers = Customer::where('active', 0)->get();
+
         $customers = Customer::all();
 
-
-        return view('internals.customers',[
-             'anothername' => $customers,         //parsing data
-        ]);
+        return view('internals.customers', compact('activeCustomers', 'inactiveCustomers')
+                      //parsing data
+        );
     }
 
     public function store()
     {
         $data= request()->validate([
             'name'=> 'required|min:3',
-            'email'=> 'required|email'
-        ]);
+            'email'=> 'required|email',
+            'active'=> 'required',
+
+            ]);
 
         $customers = new Customer();
         $customers->name = request('name');
         $customers->email = request('email');
+        $customers->active = request('active');
         $customers->save();
 
         return back();
