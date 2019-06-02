1688f06f6e0ebb4a9609ec54a072c0ed87711577
gancarajriya <gancarajriya@gmail.com>
2019-06-02 14:29:25 +0700

    use laravel scope func in model

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -9,10 +9,8 @@ class CustomersController extends Controller
 {
     public function list()
     {
-        $activeCustomers = Customer::where('active', 1)->get();
-        $inactiveCustomers = Customer::where('active', 0)->get();
-
-        $customers = Customer::all();
+        $activeCustomers = Customer::active()->get();
+        $inactiveCustomers = Customer::inactive()->get();
 
         return view('internals.customers', compact('activeCustomers', 'inactiveCustomers')
                       //parsing data
